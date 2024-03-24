9f753e77634cefb34c4f0427d88083a643c7c70c
Amir Rezvani <returnamor@gmail.com>
2024-03-24 14:04:31 +0330

    chore: datepicker input and style updated

--- src/Datepicker/index.tsx
+++ src/Datepicker/index.tsx
@@ -1,14 +1,40 @@
+import { useState, useEffect, useRef } from "react";
 import Datepicker from "./datepicker";
+import DatepickerTextfield from "./partials/input";
+import DatepickerStyleWrapper from "./style";
 
 export default function DatepickerInput() {
+  const ref = useRef<HTMLDivElement>(null);
+  const [isFocused, setIsFocuesed] = useState(false);
+
   const handleDay = (date: Date) => {
     console.log("date", date);
   };
 
+  const handleFocues = () => setIsFocuesed(true);
+
+  // handle Click outSide
+  const handleClickoutSide = () => setIsFocuesed(false);
+
+  useEffect(() => {
+    const clickedOutside = (event: MouseEvent) => {
+      const node = event?.target as HTMLElement;
+      if (!ref.current?.contains(node)) {
+        handleClickoutSide();
+      }
+    };
+    // Bind the event listener
+    document.addEventListener("mousedown", clickedOutside);
+    return () => {
+      // Unbind the event listener on clean up
+      document.removeEventListener("mousedown", clickedOutside);
+    };
+  }, [ref]);
+
   return (
-    <div>
-      <input />
-      <Datepicker onDayClicked={handleDay} />
-    </div>
+    <DatepickerStyleWrapper className="datepicker-wrapper" ref={ref}>
+      <DatepickerTextfield onClick={handleFocues} />
+      {isFocused && <Datepicker onDayClicked={handleDay} />}
+    </DatepickerStyleWrapper>
   );
 }
