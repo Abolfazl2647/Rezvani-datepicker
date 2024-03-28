679e0c645e1c045e8ce5ea5dda528ca31d957008
Amir Rezvani <returnamor@gmail.com>
2024-03-28 23:50:26 +0330

    wip: fix some types place

--- src/Datepicker/context.tsx
+++ src/Datepicker/context.tsx
@@ -2,9 +2,12 @@ import { useState, createContext, useMemo } from "react";
 import { IUtils } from "@date-io/core/IUtils";
 import GenerateDays from "./generateDays";
 
+export type Delimiter = "/" | "-";
+
 interface DatepickerProviderProps {
   children: React.ReactNode;
   dateFormat: string;
+  delimiter: Delimiter;
   datepickerAdapter: new (...args: any) => IUtils<Date>;
 }
 
@@ -12,6 +15,7 @@ interface DatepickerContextProps {
   dateAdapter: IUtils<Date>;
   days: Array<Date>;
   date: Date;
+  mask: string;
   dateFormat: string;
   nextMonth: () => void;
   prevMonth: () => void;
@@ -19,15 +23,24 @@ interface DatepickerContextProps {
 
 export const DatepickerContext = createContext({} as DatepickerContextProps);
 
+export function parseMask(string: string, delimiter: Delimiter = "/"): string {
+  const array = string.split(delimiter);
+  const newArray = array.map((part) => {
+    const newString = part.replace(/[mMyYdD]/g, "_");
+    return newString;
+  });
+  return newArray.join(delimiter);
+}
+
 export function DatepickerProvider({
   children,
   datepickerAdapter,
   dateFormat,
+  delimiter,
 }: DatepickerProviderProps) {
   const [date, setDate] = useState(new Date());
 
   const dateAdapter = new datepickerAdapter();
-  console.log("dateAdapter", dateAdapter);
 
   const days = useMemo(() => {
     return GenerateDays(dateAdapter, date);
@@ -44,9 +57,19 @@ export function DatepickerProvider({
     setDate(prevMonth);
   };
 
+  const mask = parseMask(dateFormat, delimiter);
+
   return (
     <DatepickerContext.Provider
-      value={{ days, date, dateAdapter, dateFormat, nextMonth, prevMonth }}
+      value={{
+        days,
+        date,
+        dateAdapter,
+        dateFormat,
+        mask,
+        nextMonth,
+        prevMonth,
+      }}
     >
       {children}
     </DatepickerContext.Provider>
